a0fd35eeb5bb50c3539064b2fbf2b96926b69a7e
Amirali Esfandiari <amiralinull@gmail.com>
2024-05-27 11:29:49 +0330

    i

---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-oblivion
+Homebrew Tap for Oblivion
+
+Important note: reporting issues with any of these casks should be done at their respective repositories ([Oblivion Desktop](https://github.com/bepass-org/oblivion-desktop))
+
+## Installation
+```bash
+brew tap amirali/oblivion
+```
+
+### Oblivion Desktop
+```bash
+brew install --cask oblivion-desktop
+```
---
+++ oblivion-desktop.rb
@@ -0,0 +1,11 @@
+cask "oblivion-desktop" do
+  version "0.8.18-beta"
+  sha256 "4162caa815cf7c811951278e3afcf8b2b0b4abd59861fc583acb3949d2172168"
+
+  url "https://github.com/bepass-org/oblivion-desktop/releases/download/v#{version}/oblivion-desktop-mac-x64.zip"
+  name "Oblivoin Desktop"
+  desc "Unofficial Warp client for mac"
+  homepage "https://github.com/bepass-org/oblivion-desktop/"
+
+  app "Oblivion Desktop.app"
+end
